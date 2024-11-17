870f07a03ac878029419407a28e3536d97e07913
toilaluan <tranthanhluan.nd@gmail.com>
2024-11-17 03:37:18 +0000

    update

--- neural_condense_core/validator_utils/elo.py
+++ neural_condense_core/validator_utils/elo.py
@@ -29,8 +29,14 @@ class ELOSystem:
             for j in range(i + 1, n):
                 score_i = scores[i]
                 score_j = scores[j]
-                S_i = int(score_i > score_j + 1e-2)
-                S_j = int(score_j > score_i + 1e-2)
+                diff = score_i - score_j
+                if abs(diff) < 1e-3:
+                    S_i = 0.5
+                    S_j = 0.5
+                else:
+                    S_i = int(diff > 0)
+                    S_j = int(diff < 0)
+                
                 expected_i = self.expected_score(ratings[i], ratings[j])
                 expected_j = self.expected_score(ratings[j], ratings[i])
                 rating_change_i = k_factor * (S_i - expected_i)
